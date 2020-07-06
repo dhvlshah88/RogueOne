f002897bbc7995f4ae04019e45cad3ba66ff8f26
Dhaval Shah <dhaval@travelbank.com>
2020-07-06 15:46:54 -0700

    injected url session configuration in webservice caller

--- RogueOne/Webservice/Base/SWWebServiceCaller.swift
+++ RogueOne/Webservice/Base/SWWebServiceCaller.swift
@@ -8,40 +8,39 @@ import Foundation
 import Network
 
 class SWWebServiceCaller {
-
+  
   typealias WebServiceCallerCompletion = (_ result: WebServiceResult<Data>) -> Void
-
-  static let session: URLSession = {
-    let urlSessionConfiguration = URLSessionConfiguration.default
-    let urlSession = URLSession(configuration: urlSessionConfiguration)
-    return urlSession
-  }()
-
+  
+  private let session: URLSession
+  
+  init(_ sessionConfiguration: URLSessionConfiguration = .default) {
+    session = URLSession(configuration: sessionConfiguration)
+  }
+  
   private(set) var dataTask: URLSessionDataTask?
-
+  
   static func dataResponseSerializer() -> ResponseSerializer<Data> {
     return ResponseSerializer { data, response, error in
       guard error == nil else {
         return .failure(APIError(message: error!.localizedDescription))
       }
-
-      if let response = response,
-        [204, 205].contains(response.statusCode) {
-        return .success(Data())
+      
+      guard let response = response, response.statusCode == 200 else {
+        return .failure(APIError(message: "Something went wrong while fetching response from server!"))
       }
-
+      
       guard let validData = data else {
         return .failure(APIError(message: "Data is nil!"))
       }
-
+      
       return .success(validData)
     }
   }
-
+  
   func start(request: URLRequest,
              queue: DispatchQueue,
              completion: @escaping WebServiceCallerCompletion) {
-    dataTask = SWWebServiceCaller.session.dataTask(with: request) { (data, response, error) in
+    dataTask = session.dataTask(with: request) { (data, response, error) in
       let result = SWWebServiceCaller.dataResponseSerializer().serializedResponse(data, response as? HTTPURLResponse, error)
       queue.async {
         completion(result)
